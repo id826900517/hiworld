df4905a20eca42734bfdc3835ee3ca2a0c234977
id826900517 <969046021@qq.com>
2018-03-22 21:45:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	SRC/HiWorld.java

--- SRC/HiWorld.java
+++ SRC/HiWorld.java
@@ -1,6 +1,5 @@
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("2");
+        System.out.println("1");
     }
 }
