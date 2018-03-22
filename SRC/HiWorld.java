8ecb6c2a3a2c6d3c59bec85092e31d735b6e0583
id826900517 <969046021@qq.com>
2018-03-22 21:25:14 +0800

    1

--- SRC/HiWorld.java
+++ SRC/HiWorld.java
@@ -1,6 +1,8 @@
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("q");
+        System.out.println("123");
+        System.out.println("2");
+        System.out.println("ba");
+        System.out.println("qwe");
     }
 }
